01575a4de5e34f6161a067091915c65b6d09710c
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2022-12-07 14:47:22 -0300

    ADD parenteses

--- calc1.py
+++ calc1.py
@@ -1,4 +1,4 @@
-INTEGER, PLUS, MINUS, MUL, DIV, EOF = "INTEGER", "PLUS", "MINUS", "MUL", "DIV", "EOF" #EOF end of file para indicar que não há mais input restante
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = ('INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF')
 
 class Token(object):
     def __init__(self, type, value):
@@ -78,6 +78,15 @@ class Lexer(object):
                 self.advance()
                 return Token(DIV, '/')
 
+            
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+
             self.error()
 
         return Token(EOF, None)
@@ -102,19 +111,16 @@ class Interpreter(object):
             self.error()
 
     def factor(self):
-        """
-        Retorna um INTEGER token value.
-        """
+        """factor : INTEGER | LPAREN expr RPAREN"""
         token = self.current_token
-        self.eat(INTEGER)
-        return token.value
-
-
-    def factor(self):
-        """factor : INTEGER"""
-        token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            result = self.expr()
+            self.eat(RPAREN)
+            return result
 
     def term(self):
         """term : factor ((MUL | DIV) factor)*"""
